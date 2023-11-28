2f6a622eb85269f79de1ce7a62fd0945b1063b8b
HGlass07 <>
2023-11-28 11:46:53 +0000

    added multiplication game mode calculation and score incrementing

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,6 +24,8 @@ function runGame(gameType) {
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
+    } else if (gameType === "multiply") {
+        displayMultiplyQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gametype}. Aborting!`;
@@ -46,8 +48,6 @@ function checkAnswer() {
 
     runGame(calculatedAnswer[1]);
 
-
-
 }
 
 
@@ -58,6 +58,8 @@ function calculateCorrectAnswer() {
 
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
+    } else if (operator = "x") {
+        return [operand1 * operand2, "multiply"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -91,6 +93,8 @@ function displaySubtractQuestion() {
 }
 
 
-function displayMultiplyQuestion() {
-
+function displayMultiplyQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "x";
 }
